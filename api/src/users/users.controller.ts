bdfb5453fc914f0add18e496a4c0235b46f9248e
Edouard Proust <contact@edouardproust.dev>
2026-02-21 13:43:50 +0100

    finished base crud + installed PartialType lib in node

--- api/src/users/users.controller.ts
+++ api/src/users/users.controller.ts
@@ -1,34 +1,50 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
-import { CreateUserDto } from './dtos/CreateUserDto';
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	HttpCode,
+	HttpStatus,
+	Param,
+	ParseIntPipe,
+	Patch,
+	Post,
+} from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('users')
-export class UsersController
-{
+export class UsersController {
 	constructor(private readonly service: UsersService) {}
-	
+
 	@Get()
-	findAll() {
+	async findAll() {
 		return this.service.findAll();
 	}
 
 	@Get(':id')
-	findOne(@Param('id', ParseIntPipe) id: number) {
+	async findOne(@Param('id', ParseIntPipe) id: number) {
 		return this.service.findOneById(id);
 	}
 
 	@Post()
-	createOne(@Body() userData: CreateUserDto) {
-		return this.service.createOne(userData);
+	@HttpCode(HttpStatus.CREATED)
+	async createOne(@Body() createUserDto: CreateUserDto) {
+		return this.service.createOne(createUserDto);
 	}
 
 	@Delete(':id')
-	deleteOne(@Param('id', ParseIntPipe) id: number) {
+	@HttpCode(HttpStatus.NO_CONTENT)
+	async deleteOne(@Param('id', ParseIntPipe) id: number) {
 		return this.service.deleteOne(id);
 	}
 
-	@Put(':id')
-	updateOne(@Param('id') id: string, @Body() userData: CreateUserDto) {
-		return `Update user with id ${id} with data: ${JSON.stringify(userData)}`;
+	@Patch(':id')
+	async updateOne(
+		@Param('id', ParseIntPipe) id: number,
+		@Body() updateUserDto: UpdateUserDto,
+	) {
+		return this.service.updateOneById(id, updateUserDto);
 	}
 }
