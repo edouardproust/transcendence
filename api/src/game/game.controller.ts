fa6d2cc67b072b436e492bb22a20040924bbb98a
ahetru <axel.a.hetru@gmail.com>
2026-03-08 16:27:10 +0100

    test(game): add basic unit tests for controller and service

--- api/src/game/game.controller.ts
+++ api/src/game/game.controller.ts
@@ -4,10 +4,10 @@ import { CreateGameDto } from './dto/create-game.dto';
 
 @Controller('game')
 export class GameController {
-	constructor(private readonly gameSerice: GameService) {}
+	constructor(private readonly gameService: GameService) {}
 
 	@Post()
 	createGame(@Body() dto: CreateGameDto) {
-		return this.gameSerice.createGame(dto);
+		return this.gameService.createGame(dto);
 	}
 }
