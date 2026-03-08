fbde601df572cb5af282bc584c3a88ec2a18f155
ahetru <axel.a.hetru@gmail.com>
2026-03-08 16:27:10 +0100

    test(game): add basic unit tests for controller and service

--- api/src/game/game.controller.spec.ts
+++ api/src/game/game.controller.spec.ts
@@ -1,18 +1,52 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GameController } from './game.controller';
+import { GameService } from './game.service';
 
 describe('GameController', () => {
-  let controller: GameController;
+	let controller: GameController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [GameController],
-    }).compile();
+	const mockGameService = {
+		createGame: jest.fn().mockReturnValue({
+			message: 'Game created',
+			players: {
+				white: 1,
+				black: 2,
+			},
+		}),
+	};
 
-    controller = module.get<GameController>(GameController);
-  });
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			controllers: [GameController],
+			providers: [
+				{
+					provide: GameService,
+					useValue: mockGameService,
+				},
+			],
+		}).compile();
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+		controller = module.get<GameController>(GameController);
+	});
+
+	it('should be defined', () => {
+		expect(controller).toBeDefined();
+	});
+
+	it('should create a game', () => {
+		const result = controller.createGame({
+			whiteId: 1,
+			blackId: 2,
+		});
+
+		expect(result).toEqual({
+			message: 'Game created',
+			players: {
+				white: 1,
+				black: 2,
+			},
+		});
+
+		expect(mockGameService.createGame).toHaveBeenCalled();
+	});
 });
--- api/src/game/game.service.spec.ts
+++ api/src/game/game.service.spec.ts
@@ -1,18 +1,42 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { BadRequestException } from '@nestjs/common';
 import { GameService } from './game.service';
 
 describe('GameService', () => {
-  let service: GameService;
+	let service: GameService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [GameService],
-    }).compile();
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [GameService],
+		}).compile();
 
-    service = module.get<GameService>(GameService);
-  });
+		service = module.get<GameService>(GameService);
+	});
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+	it('should be defined', () => {
+		expect(service).toBeDefined();
+	});
+	it('should create a game', () => {
+		const result = service.createGame({
+			whiteId: 1,
+			blackId: 2,
+		});
+
+		expect(result).toEqual({
+			message: 'Game created',
+			players: {
+				white: 1,
+				black: 2,
+			},
+		});
+	});
+
+	it('should throw if players are the same', () => {
+		expect(() =>
+			service.createGame({
+				whiteId: 1,
+				blackId: 1,
+			}),
+		).toThrow(BadRequestException);
+	});
 });
