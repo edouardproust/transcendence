245b9970cf93fd50cf78ea3e460e99f1d6db967d
ahetru <axel.a.hetru@gmail.com>
2026-03-08 15:06:47 +0100

    feat(game): create base game module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { GameModule } from './game/game.module';
 
 @Module({
-	imports: [UsersModule, AuthModule],
+	imports: [UsersModule, AuthModule, GameModule],
 })
 export class AppModule {}
