4e5b20b7b423a87b840ae7544a4ead24bd27c0af
ahetru <axel.a.hetru@gmail.com>
2026-03-08 15:36:24 +0100

    feat(game): implement create game endpoint

--- api/src/game/game.controller.ts
+++ api/src/game/game.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { GameService } from './game.service';
+import { CreateGameDto } from './dto/create-game.dto';
 
 @Controller('game')
-export class GameController {}
+export class GameController {
+	constructor(private readonly gameSerice: GameService) {}
+
+	@Post()
+	createGame(@Body() dto: CreateGameDto) {
+		return this.gameSerice.createGame(dto);
+	}
+}
--- api/src/game/game.service.ts
+++ api/src/game/game.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { CreateGameDto } from './dto/create-game.dto';
+import { distinct } from 'rxjs';
 
 @Injectable()
-export class GameService {}
+export class GameService {
+	createGame(dto: CreateGameDto) {
+		return {
+			message: 'Game created',
+			players: {
+				ehite: dto.whiteId,
+				black: dto.blackId,
+			},
+		};
+	}
+}
